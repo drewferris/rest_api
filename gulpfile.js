259a415140b16785ba637ee65609c729e475be7c
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-06-19 14:19:42 -0700

    changed gulpfile.js and updated dependencies in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,67 @@
+'use strict';
 const gulp = require('gulp');
-const eslint = require('gulp-eslint');
-const mocha = require('gulp-mocha');
+const webpack = require('webpack-stream');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
 
-gulp.task('lint', () => {
-  gulp.src(['./*.js', './test/*.js'])
-        .pipe(eslint({
-          env: {}
-        }))
-        .pipe(eslint.format());
+var paths = {
+  dev: {
+    css: 'app/css/**/*.css',
+    html: 'app/**/*.html',
+    js: 'app/js/**/*.js',
+    test: 'test/*_test.js'
+  },
+  build: {
+    main: 'build/',
+    css: 'build/css',
+    js: 'build/js',
+    test: 'test/'
+  }
+};
+
+gulp.task('watch', function () {
+  gulp.watch(paths.dev.html, ['statichtmlfiles:dev']);
+  gulp.watch(paths.dev.js, ['bundle']);
+  gulp.watch(paths.dev.css, ['staticcssfiles:dev']);
 });
 
-gulp.task('mocha', () => {
-  return gulp.src('./test/*.js', {
-    read: false
-  })
-        .pipe(mocha({
-          reporter: 'nyan'
-        }));
+gulp.task('test:watch', function () {
+  gulp.watch(paths.dev.test, ['bundle:test']);
 });
 
-gulp.task('watch', () => {
-  gulp.watch(['./*.js', './test/*.js' ], ['mocha', 'lint']);
+gulp.task('statichtmlfiles:dev', () => {
+  return gulp.src(paths.dev.html)
+    .pipe(gulp.dest(paths.build.main));
 });
 
-gulp.task('default', ['mocha', 'lint', 'watch']);
+gulp.task('staticcssfiles:dev', () => {
+  return gulp.src(paths.dev.css)
+    .pipe(gulp.dest(paths.build.main));
+});
+
+
+
+gulp.task('bundle', () => {
+  return gulp.src(__dirname + '/app/js/client.js')
+    .pipe(plumber({
+      errorHandler: notify.onError('Error: <%= error.message %>')
+    }))
+    .pipe(webpack({
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    .pipe(gulp.dest(paths.build.main));
+});
+
+gulp.task('bundle:test', () => {
+  return gulp.src(paths.dev.test)
+    .pipe(webpack({
+      output: {
+        filename: 'test_bundle.js'
+      }
+    }))
+    .pipe(gulp.dest(paths.build.test));
+});
+
+gulp.task('default', ['bundle', 'statichtmlfiles:dev', 'staticcssfiles:dev']);
